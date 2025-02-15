39130193b63855c252231957b14863676c7a4d87
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2025-02-15 20:02:25 +0200

    fix index js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,3 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
+import './js/API/api'
 import './js/components/header'
-=======
->>>>>>> Stashed changes
-import { CocktailAPI } from './js/API/api.js';
-import { CocktailComponents } from './/js/components/renderCoctails.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-  const cocktailsList = document.querySelector('.cocktails__list');
-  
-  try {
-    const data = await CocktailAPI.fetchNonAlcoholicCocktails();
-    CocktailComponents.renderCocktailsList(cocktailsList, data.drinks);
-  } catch (error) {
-    console.error(error);
-    cocktailsList.innerHTML = `
-      <li class="error-message">
-        Failed to load cocktails. Please try again later.
-      </li>
-    `;
-  }
-<<<<<<< Updated upstream
-});
-=======
-});
->>>>>>> 6695daee9d6754ce71791bc059e1ece3658e3b22
->>>>>>> Stashed changes
+import './js/components/renderCoctails'
