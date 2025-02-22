d1a84f54c63a94ec3e3faac8d6f7a31596a29445
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2025-02-22 16:26:15 +0200

    done

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,35 +1,44 @@
-const themeBtn = document.querySelector('#theme-checkbox')
+const themeBtn = document.querySelectorAll('.theme-checkbox')
 const body = document.querySelector('body')
 
 const menuBtn = document.querySelector('.header__menu-btn')
 const menuList = document.querySelector('.header__menu-list')
 
+const menuBtnMob = document.querySelector('.header-menu__menu-btn')
+const menuListMob = document.querySelector('.header-menu__menu-list')
+
+const openBurgerBtn = document.querySelector('.header__mobile-menu-btn')
+const headerMenu = document.querySelector('.header-menu')
+const closeBurgerBtn = document.querySelector('.header-menu__close')
+
 Object.keys(localStorage).forEach(key => {
     if (key.includes('theme')) {
         const parsedObj = (JSON.parse(localStorage.getItem(key)))
-        console.log(parsedObj)
 
         if (localStorage.getItem(key) === null || parsedObj.themeColor === 'black') {
 
             document.documentElement.style.setProperty('--black', '#FCFCFC');
             document.documentElement.style.setProperty('--white', '#212121');
             body.style.backgroundColor = '#202025';
-            themeBtn.checked = parsedObj.inputValue
+            themeBtn[0].checked = parsedObj.inputValue
+            themeBtn[1].checked = parsedObj.inputValue
+
 
             let jsonValuesTheme = {
                 themeColor: "black",
-                inputValue: themeBtn.checked
+                inputValue: themeBtn[0].checked
             }
             localStorage.setItem(key, JSON.stringify(jsonValuesTheme))
         } else if (parsedObj.themeColor === 'white') {
             document.documentElement.style.setProperty('--black', '#212121');
             document.documentElement.style.setProperty('--white', '#FCFCFC');
             body.style.backgroundColor = '';
-            themeBtn.checked = parsedObj.inputValue
+            themeBtn[0].checked = parsedObj.inputValue
+            themeBtn[1].checked = parsedObj.inputValue
 
             let jsonValuesTheme = {
                 themeColor: "white",
-                inputValue: themeBtn.checked
+                inputValue: themeBtn[0].checked
             }
 
             localStorage.setItem('theme', JSON.stringify(jsonValuesTheme))
@@ -39,30 +48,59 @@ Object.keys(localStorage).forEach(key => {
     }
 })
 
-themeBtn.addEventListener("click", () => {
-    if (localStorage.getItem('theme') === null || JSON.parse(localStorage.getItem('theme')).themeColor === 'white') {
-        document.documentElement.style.setProperty('--black', '#FCFCFC');
-        document.documentElement.style.setProperty('--white', '#212121');
-        body.style.backgroundColor = '#202025';
-
-        let jsonValuesTheme = {
-            themeColor: "black",
-            inputValue: themeBtn.checked
+themeBtn.forEach(button =>{
+    button.addEventListener("click", () => {
+        console.log('z')
+        if (localStorage.getItem('theme') === null || JSON.parse(localStorage.getItem('theme')).themeColor === 'white') {
+            document.documentElement.style.setProperty('--black', '#FCFCFC');
+            document.documentElement.style.setProperty('--white', '#212121');
+            body.style.backgroundColor = '#202025';
+    
+            let jsonValuesTheme = {
+                themeColor: "black",
+                inputValue: button.checked
+            }
+            localStorage.setItem('theme', JSON.stringify(jsonValuesTheme))
+        } else if (JSON.parse(localStorage.getItem('theme')).themeColor === 'black') {
+            document.documentElement.style.setProperty('--black', '#212121');
+            document.documentElement.style.setProperty('--white', '#FCFCFC');
+            body.style.backgroundColor = '';
+    
+            let jsonValuesTheme = {
+                themeColor: "white",
+                inputValue: button.checked
+            }
+    
+            localStorage.setItem('theme', JSON.stringify(jsonValuesTheme))
         }
-        localStorage.setItem('theme', JSON.stringify(jsonValuesTheme))
-    } else if (JSON.parse(localStorage.getItem('theme')).themeColor === 'black') {
-        document.documentElement.style.setProperty('--black', '#212121');
-        document.documentElement.style.setProperty('--white', '#FCFCFC');
-        body.style.backgroundColor = '';
+    });
+})
+// themeBtn.addEventListener("click", () => {
+//     console.log('z')
+//     if (localStorage.getItem('theme') === null || JSON.parse(localStorage.getItem('theme')).themeColor === 'white') {
+//         document.documentElement.style.setProperty('--black', '#FCFCFC');
+//         document.documentElement.style.setProperty('--white', '#212121');
+//         body.style.backgroundColor = '#202025';
 
-        let jsonValuesTheme = {
-            themeColor: "white",
-            inputValue: themeBtn.checked
-        }
+//         let jsonValuesTheme = {
+//             themeColor: "black",
+//             inputValue: themeBtn.checked
+//         }
+//         localStorage.setItem('theme', JSON.stringify(jsonValuesTheme))
+//     } else if (JSON.parse(localStorage.getItem('theme')).themeColor === 'black') {
+//         document.documentElement.style.setProperty('--black', '#212121');
+//         document.documentElement.style.setProperty('--white', '#FCFCFC');
+//         body.style.backgroundColor = '';
 
-        localStorage.setItem('theme', JSON.stringify(jsonValuesTheme))
-    }
-});
+//         let jsonValuesTheme = {
+//             themeColor: "white",
+//             inputValue: themeBtn.checked
+//         }
+
+//         localStorage.setItem('theme', JSON.stringify(jsonValuesTheme))
+//     }
+// });
+
 
 menuBtn.addEventListener('click', () => {
     if (menuList.classList.value.includes('hidden-menu-list')) {
@@ -70,4 +108,21 @@ menuBtn.addEventListener('click', () => {
     } else if (menuList.classList.value.includes('hidden-menu-list') === false) {
         menuList.classList.add('hidden-menu-list')
     }
+})
+
+menuBtnMob.addEventListener('click', () => {
+    if (menuListMob.classList.value.includes('hidden-menu-list')) {
+        menuListMob.classList.remove('hidden-menu-list')
+        document.querySelector('.header-menu__menu-btn-svg').style.transform = 'rotate(180deg)'
+    } else if (menuListMob.classList.value.includes('hidden-menu-list') === false) {
+        menuListMob.classList.add('hidden-menu-list')
+        document.querySelector('.header-menu__menu-btn-svg').style.transform = 'rotate(0)'
+    }
+})
+
+openBurgerBtn.addEventListener('click',() =>{
+    headerMenu.classList.remove('hidden-burger-menu')
+})
+closeBurgerBtn.addEventListener('click', () => {
+    headerMenu.classList.add('hidden-burger-menu')
 })
