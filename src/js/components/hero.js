e2a401b03d935c33f1fa168e1e3300d91f35161a
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2025-03-01 20:00:37 +0200

    fix

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -3,21 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const input = document.querySelector(".header__input");
   const btnOpen = document.querySelector('.hero__open-letters svg');
   const mobBoxBtn = document.querySelector('.hero__mob-box');
-  
+
   if (list && input) {
     list.addEventListener("click", (e) => {
       input.value += e.target.textContent;
     });
   }
-  
+
   if (btnOpen && mobBoxBtn) {
     btnOpen.addEventListener('click', () => {
       if (mobBoxBtn.classList.contains('mob-box-hidden')) {
         mobBoxBtn.classList.remove('mob-box-hidden');
         btnOpen.style.transform = 'rotate(180deg)';
+        document.querySelector('.hero').style.paddingBottom = 55 + 'px'
       } else {
         mobBoxBtn.classList.add('mob-box-hidden');
         btnOpen.style.transform = 'rotate(0)';
+        document.querySelector('.hero').style.paddingBottom = 0
       }
     });
   }
